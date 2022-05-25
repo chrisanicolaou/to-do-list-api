6998ac9218a2a8dbb8c68f42030d41930f57dc70
Christos Nicolaou <chris.anicolaou@yahoo.co.uk>
2022-05-25 01:09:10 +0100

    I don't know what I did, but it's working now

--- Db/Connection.cs
+++ Db/Connection.cs
@@ -92,8 +92,6 @@ public class Connection
             string decodedBinarySecret = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
             secret = decodedBinarySecret;
         }
-
-        Console.WriteLine($"Secret successfully decrypted: {secret}");
         
         # nullable disable
         return JsonConvert.DeserializeObject<Connection>(secret);
